bcc9094c05aefa1e6eacceb8b7aa99601dfc6f77
Bharat <bharatappguru@gmail.com>
2015-06-24 14:13:18 +0530

    Reflect Object In UNITY(2D) Tutorial

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+
+    #region PUBLIC-PROPERTIES
+    public float bulletSpeed;
+    #endregion
+
+    #region PRIVATE-PROPERTIES
+    #endregion
+
+    #region UNITY-CALLBACKS
+    void OnEnable()
+    {
+        rigidbody2D.velocity = transform.right * bulletSpeed;
+    }
+	
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Wall"))
+            CollisionWithWall(other);
+    }
+    #endregion
+
+    #region PRIVATE-MEHODS
+    void CollisionWithWall(Collision2D other)
+    {
+        Vector3 reflectedPosition = Vector3.Reflect(transform.right, other.contacts[0].normal);
+        rigidbody2D.velocity = (reflectedPosition).normalized * bulletSpeed;
+
+        Vector3 dir = rigidbody2D.velocity;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        rigidbody2D.MoveRotation(angle);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/SetAim.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetAim : MonoBehaviour {
+
+    public Transform target;
+
+    public Bullet bullet;
+
+    public bool canFire;
+	// Use this for initialization
+	void Start () {
+        canFire = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        ProcessInput();
+	}
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && canFire)
+            SetTarget();
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+            Fire();
+    }
+
+    void SetTarget()
+    {
+        Vector3 tempTargetPosition;
+
+        target.gameObject.SetActive(true);
+
+        tempTargetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        tempTargetPosition.z = 0;
+        target.transform.position = tempTargetPosition;
+
+        Vector3 position = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 direction = Input.mousePosition - position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        
+    }
+
+    void Fire()
+    {
+        canFire = false;
+        target.gameObject.SetActive(false);
+        bullet.enabled = true;
+    }
+}
